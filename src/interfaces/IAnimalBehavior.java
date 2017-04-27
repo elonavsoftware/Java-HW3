9a467fde5be2b7957fba9c84932aead6292ffd8b
User <User@Fujitsu>
2017-04-27 16:33:41 +0300

    I'v added a new interface.

--- src/interfaces/IAnimalBehavior.java
+++ src/interfaces/IAnimalBehavior.java
@@ -1,5 +1,12 @@
 package interfaces;
 
-public class IAnimalBehavior {
-
-}
+public interface IAnimalBehavior {
+    public String getAnimalName();
+	public int getSize();
+	public void eatInc();
+	public int getEatCount();
+	public boolean getChanges ();
+	public void setSuspended();
+	public void setResumed();
+	public void setChanges (boolean state);
+}//inteface IAnimalBehavior
