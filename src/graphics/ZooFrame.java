5bc6876e092559ec52b056fbbbf870993de4fdd2
USER <USER@EA>
2017-05-03 16:51:38 +0300

    Elon & Mahdi

--- src/graphics/ZooFrame.java
+++ src/graphics/ZooFrame.java
@@ -1,17 +1,25 @@
 package graphics;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
-import com.sun.xml.internal.ws.dump.LoggingDumpTube.Position;
 
 public class ZooFrame extends JFrame
 {
+	private static final long serialVersionUID = 1L;
 	private final static String ZOO_FRAME_TEXT = "Zoo";
 	static ZooPanel MyZooPanel;
 	public static void main(String[] args)
@@ -27,6 +35,7 @@ public class ZooFrame extends JFrame
 
 class MyPanel extends JPanel implements ActionListener
 {
+	private static final long serialVersionUID = 1L;
 	private final static String MENU_TEXT1 = "File";
 	private final static String MENU_TEXT2 = "Background";
 	private final static String MENU_TEXT3 = "Help";
@@ -37,61 +46,109 @@ class MyPanel extends JPanel implements ActionListener
 	private final static String BUTTON_TEXT5 = "Food";
 	private final static String BUTTON_TEXT6 = "Info";
 	private final static String BUTTON_TEXT7 = "Exit";
-	JMenu menu1, menu2, menu3;
-	JButton button1, button2, button3, button4, button5, button6, button7;
+	private static final String BACKGROUND_PATH = "C://";
+	JPanel panel1, panel2;
+	JMenuBar menuBar;
+	JMenu file, background, help;
+	JMenuItem f_exit, b_image, b_green, b_none, h_help;
+	JButton addAnimal, sleep, wakeUp, clear, food, info, exit;
 	public MyPanel()
 	{
 		BorderLayout myBorderLayout = new BorderLayout();
 		//Sets the LayoutManager to the Layout
 		this.setLayout(myBorderLayout);
-		JPanel panel1 = new JPanel();
-		JPanel panel2 = new JPanel();
 		
-		menu1 = new JMenu(MENU_TEXT1);
-		menu2 = new JMenu(MENU_TEXT2);
-		menu3 = new JMenu(MENU_TEXT3);
-		button1 = new JButton(BUTTON_TEXT1);
-		button2 = new JButton(BUTTON_TEXT2);
-		button3 = new JButton(BUTTON_TEXT3);
-		button4 = new JButton(BUTTON_TEXT4);
-		button5 = new JButton(BUTTON_TEXT5);
-		button6 = new JButton(BUTTON_TEXT6);
-		button7 = new JButton(BUTTON_TEXT7);
+		panel1 = new JPanel();
+		panel2 = new JPanel();
+		menuBar = new JMenuBar();
+		
+		f_exit = new JMenuItem("Exit");
+		b_image = new JMenuItem("Image");
+		b_green = new JMenuItem("Green");
+		b_none = new JMenuItem("None");
+		h_help = new JMenuItem("Help");
+		
+		file = new JMenu(MENU_TEXT1);
+		file.add(f_exit);
+		background = new JMenu(MENU_TEXT2);
+		background.add(b_image);
+		background.add(b_green);
+		background.add(b_none);
+		help = new JMenu(MENU_TEXT3);
+		help.add(h_help);
+		
+		addAnimal = new JButton(BUTTON_TEXT1);
+		sleep = new JButton(BUTTON_TEXT2);
+		wakeUp = new JButton(BUTTON_TEXT3);
+		clear = new JButton(BUTTON_TEXT4);
+		food = new JButton(BUTTON_TEXT5);
+		info = new JButton(BUTTON_TEXT6);
+		exit = new JButton(BUTTON_TEXT7);
 		 
 		panel1.setLayout(new FlowLayout());
 		panel2.setLayout(new FlowLayout());
 		
 		//up-left
-		panel1.add(menu1);
-		panel1.add(menu2);
-		panel1.add(menu3);
+		menuBar.add(file);
+		menuBar.add(background);
+		menuBar.add(help);
+		
+		panel1.add(menuBar);
 		
 		//down-left
-		panel2.add(button1);
-		panel2.add(button2);
-		panel2.add(button3);
-		panel2.add(button4);
-		panel2.add(button5);
-		panel2.add(button6);
-		panel2.add(button7);
+		panel2.add(addAnimal);
+		panel2.add(sleep);
+		panel2.add(wakeUp);
+		panel2.add(clear);
+		panel2.add(food);
+		panel2.add(info);
+		panel2.add(exit);
 		
 		this.add(panel1, BorderLayout.NORTH);
 		this.add(panel2, BorderLayout.SOUTH);
-	}
+		this.setBackground(Color.WHITE);
 		
+		this.f_exit.addActionListener(this);
+		this.b_image.addActionListener(this);
+		this.b_green.addActionListener(this);
+		this.b_none.addActionListener(this);
+		this.h_help.addActionListener(this);
+		this.exit.addActionListener(this);
+	}
+	@Override
 	public void actionPerformed(ActionEvent event)
 	{
-		if (event.getSource() == menu1)
+		if (event.getSource() == f_exit)
 		{
-			//menu1.add("Exit");
+			System.exit(0);	
 		}
-		if (event.getSource() == menu2)
+		if (event.getSource() == b_image)
 		{
-			
+			BufferedImage img = null;
+			try
+			{
+				img = ImageIO.read(new File(BACKGROUND_PATH));
+			}
+			catch (IOException e)
+			{
+				System.out.println("Error!");
+			}	
 		}
-		if (event.getSource() == menu3)
+		if (event.getSource() == b_green)
 		{
-			
+			this.setBackground(Color.GREEN);
+		}
+		if (event.getSource() == b_none)
+		{
+			this.setBackground(Color.WHITE);
+		}
+		if (event.getSource() == h_help)
+		{
+			JOptionPane.showMessageDialog(this, "Home Work 3\nGUI @ Threads");
+		}
+		if (event.getSource() == exit)
+		{
+			System.exit(0);
 		}
 	}
 } //class MyPanel extends JPanel implements ActionListener
